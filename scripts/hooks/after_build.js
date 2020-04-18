089d6ea857a94bb311c5dc840b2250961a263f78
Sumedh <sumedh@Sumedhs-MacBook-Air.local>
2020-04-18 11:19:45 +0530

    reverting after build

--- scripts/hooks/after_build.js
+++ scripts/hooks/after_build.js
@@ -1,34 +1,13 @@
 /**
-  Copyright (c) 2015, 2018, Oracle and/or its affiliates.
+  Copyright (c) 2015, 2020, Oracle and/or its affiliates.
   The Universal Permissive License (UPL), Version 1.0
 */
+
 'use strict';
-const fs = require('fs');
-const archiver = require('archiver');
+
 module.exports = function (configObj) {
   return new Promise((resolve, reject) => {
-   console.log("Running after_build hook.");
-
-    //change the extension of the my-archive.xxx file from .war to .zip as needed
-    const output = fs.createWriteStream('my-archive.war');
-    //leave unchanged, compression is the same for WAR or Zip file
-    const archive = archiver('zip');
-  
-    output.on('close', () => {
-      console.log('Files were successfully archived.');
-      resolve();
-    });
-  
-    archive.on('warning', (error) => {
-      console.warn(error);
-    });
-  
-    archive.on('error', (error) => {
-      reject(error);
-    });
-  
-    archive.pipe(output);
-    archive.directory('web', false);
-    archive.finalize();
+  	console.log("Running after_build hook.");
+  	resolve(configObj);
   });
 };
